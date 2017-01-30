638b788d18d4eb1971dd1a2a38beeff2668a0ed0
Michael Grijalva <mgrijalva@bitglass.com>
2017-01-30 15:36:57 -0800

    Update pypi setup. Add changelog

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(
     name='nose2_html_report',
     packages=['nose2_html_report'],
-    version='0.0.1',
+    version='0.1.0',
     description='Generate an HTML report for your unit tests',
+    long_description=open('README.md', 'r').read(),
     author='Michael Grijalva',
     author_email='michaelgrij@gmail.com',
     license='MIT',
@@ -12,5 +13,12 @@ setup(
     url='https://github.com/mgrijalva/nose2-html-report',
     download_url='',
     keywords=['nose2', 'testing', 'reporting'],
-    classifiers=[]
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
 )
